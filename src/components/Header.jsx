1aa7d0397a6519cb36e18bb9c2dbbd82d1b0dc09
ZennithYattare <luis.espera.0758@gmail.com>
2023-03-21 23:49:19 +0800

    added href

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,20 +3,32 @@
 import React from "react";
 
 const Header = () => {
+	const [count, setCount] = React.useState(0);
+
 	return (
 		<header className="mb-12 flex h-20 shadow-[0px_2px_5px_rgba(0,0,0,0.2)]">
 			<div
 				className="m-auto flex align-middle hover:cursor-pointer"
-				onClick={() => console.log("I was clicked")}
+				onClick={() => setCount((count) => count + 1)}
 			>
 				<img
 					src="https://zennithyattare.github.io/productPage/vite.svg"
 					className="inline-block"
 					alt="Vite logo"
 				/>
-				<span className="ml-2 select-none text-3xl font-semibold">
-					Vite
-				</span>
+				{count <= 6 ? (
+					<span className="ml-2 select-none text-3xl font-semibold">
+						Vite
+					</span>
+				) : (
+					<a
+						href="https://youtu.be/dQw4w9WgXcQ"
+						target={"_blank"}
+						className="ml-2 select-none text-3xl font-semibold"
+					>
+						Vite
+					</a>
+				)}
 			</div>
 		</header>
 	);
